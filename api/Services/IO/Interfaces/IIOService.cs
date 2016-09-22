8b22eea01615d29f5710247d49a7266906bb07f9
Vladimir Stark <stark@2n.cz>
2016-09-22 15:20:53 +0200

    implemented IO service

---
+++ api/Services/IO/Interfaces/IIOService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HIPHttpApi
+{
+    public interface IIOService : IService
+    {
+        IResponse Caps(string port);
+        IResponse Status(string port);
+        IResponse Ctrl(string port, IOPortAction action, string response);
+    }
+}
