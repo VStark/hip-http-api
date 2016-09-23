1a6661328273c02e26b767c8b98b42e1e07753c8
Vladimir Stark <stark@2n.cz>
2016-09-23 15:05:51 +0200

    implemented tests for Camera service

--- tests/Properties/Resources.Designer.cs
+++ tests/Properties/Resources.Designer.cs
@@ -83,6 +83,26 @@ namespace HIPHttpApiTest.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
+        internal static byte[] CameraCaps {
+            get {
+                object obj = ResourceManager.GetObject("CameraCaps", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap CameraSnapshot {
+            get {
+                object obj = ResourceManager.GetObject("CameraSnapshot", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
         internal static byte[] Caps {
             get {
                 object obj = ResourceManager.GetObject("Caps", resourceCulture);
